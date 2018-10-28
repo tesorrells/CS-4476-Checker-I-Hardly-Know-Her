77fdac688d49faa99df75ca1c397f6f80317baa5
joshreno <joshuagregr@gmail.com>
2018-10-27 21:35:16 -0400

    added board dataset

--- detectCircles.py
+++ detectCircles.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-frame = cv2.imread("SCO28011_lg.jpg")
+frame = cv2.imread("checkerboard.jpg")
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray,(5,5),0);
 gray = cv2.medianBlur(gray,5)

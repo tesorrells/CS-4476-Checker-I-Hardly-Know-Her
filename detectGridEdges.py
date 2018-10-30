58e5263a4d35399fd8ed2cbcb0dae62fd700834f
tesorrells <trevor.sorrells45@gmail.com>
2018-10-30 16:15:02 -0400

    Update detectGridEdges.py

--- detectGridEdges.py
+++ detectGridEdges.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+# image to detect
 img = cv.imread("IMG_4075.jpg",0)
+# optimal Canny params
 edges = cv.Canny(img,100,100)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])

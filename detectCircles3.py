2b2985dd5297d2eae72cd23234c28637d90a24fd
joshreno <joshuagregr@gmail.com>
2018-11-29 15:19:17 -0500

    should be final circle detector

--- detectCircles3.py
+++ detectCircles3.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 from glob import glob
 
-def detectCircles(directory, showImage = True):
+def detectCircles(directory, showImage = True, rectangle = False):
 	img = cv2.imread(directory, 0)
 	img = cv2.medianBlur(img, 1)
 	img2 = cv2.imread(directory)
 	circleCenters = []
 	redCenters = []
 	blackCenters = []
-	circles = np.uint16(np.around(cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,10,param1=50,param2=100,minRadius=50,maxRadius=150)))
+	circles = np.uint16(np.around(cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,10,param1=55,param2=100,minRadius=50,maxRadius=150)))
 	for i in circles[0,:]:
 	    chunk = img2[i[1] - i[2]/2: i[1] + i[2]/2, i[0] - i[2]/2: i[0] + i[2]/2, :]
 	    pixels = chunk[:][:]
@@ -24,21 +24,22 @@ def detectCircles(directory, showImage = True):
 	    	green /= len(pixels[2])
 	    	red /= len(pixels[2])
 	    except:
-	   		print("help")
-	    cv2.rectangle(img2, (i[0] - i[2]/2, i[1] - i[2]/2), (i[0] + i[2]/2, i[1] + i[2]/2), (255, 255, 0), 1)
+	   		print("problem with piece")
+	    if rectangle:
+	    	cv2.rectangle(img2, (i[0] - i[2]/2, i[1] - i[2]/2), (i[0] + i[2]/2, i[1] + i[2]/2), (255, 255, 0), 1)
 	    if red > 70:
-	    	cv2.circle(img2,(i[0],i[1]),i[2],(255,255,0),5)
+	    	cv2.circle(img2,(i[0],i[1]),i[2],(100,100,255),5)
 	    	redCenters.append((i[0], i[1]))
 	    else:
-	    	#cv2.circle(img2,(i[0],i[1]),i[2],(255,255,0),5)
+	    	cv2.circle(img2,(i[0],i[1]),i[2],(255,255,0),5)
 	    	blackCenters.append((i[0], i[1]))
 	    circleCenters.append((i[0], i[1]))
 	img2 = cv2.resize(img2, (612, 816))
-	# cv2.rectangle(img2, (1940, 887), (3200, 2000), (100, 100, 200), 5)
 	if showImage:
 		cv2.imshow('circles', img2)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
 	return redCenters, blackCenters
 
-detectCircles("circleDetection/IMG_4075.png", True)
+#detectCircles("circleDetection/IMG_4075.png", True)
+#detectCircles("checkers/board1/IMG_4075.JPG", True)
